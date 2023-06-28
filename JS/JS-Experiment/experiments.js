f2082936dfea5ffe1318c8ed7fd3135283da5cb6
Abel-B3lla <bel8794@gmail.com>
2023-06-28 22:55:47 +0300

    JS Experiment

--- JS/JS-Experiment/experiments.js
+++ JS/JS-Experiment/experiments.js
@@ -1,1 +1,28 @@
 console.log("Hello from JS");
+const mr = 4000;
+const yr = mr *12;
+console.log(yr)
+
+const myname = `Bella's`
+console.log(myname)
+
+const firstname = `Abel`
+const fathersname = `Ali`
+const sentence = `I go by the name ` + firstname + ` ` + fathersname + ` A.K.A Bella`
+const sentence2 = `I go by the name ${firstname} ${fathersname} A.K.A Bella`
+console.log(sentence)
+console.log(sentence2)
+
+let BellaIsChis = true
+
+if (BellaIsChis) {
+  console.log (`Bella is Chis`)}
+  else {
+    console.log(`C'mon dude Bella is alwasy chis`)
+}
+
+let BellasWeight = 51
+
+if (BellasWeight === 52.5) { console.log(`Emmm okay`)}
+else if (BellasWeight > 52.5){console.log('The world is come to an end')}
+else {console.log(`Can a person go skinny than this?`)}
